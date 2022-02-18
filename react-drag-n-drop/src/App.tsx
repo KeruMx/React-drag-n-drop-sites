81fc74db2a3951698f43b81f0a7b03218f4b23a8
KeruMx <eemmanuel.lunaa@gmail.com>
2022-02-17 21:54:17 -0600

    PruebasCommit

--- react-drag-n-drop/src/App.tsx
+++ react-drag-n-drop/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Kru
+          Learn React with Kru asdkjklsdaljlads asjkñkldaskjlkads lñ
         </a>
       </header>
     </div>
